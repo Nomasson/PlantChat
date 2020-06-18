0f010d920d71a80d2c465731fcdb653b57da1d3f
Nomasson <44348196+Nomasson@users.noreply.github.com>
2020-06-18 13:02:43 +0300

    Update config.py
    Updated API key

--- plantbot/config.py
+++ plantbot/config.py
@@ -4,6 +4,6 @@ DEBUG = True
 MONGO_URI = 'mongodb+srv://admin:admin1234@cluster0-rsh71.mongodb.net/test?retryWrites=true&w=majority' 
 ALLOWED_IMAGE_EXTENSIONS = ["JPEG", "JPG", "PNG"]
 MAX_IMAGE_FILESIZE = 100 * 1024 * 1024
-API_KEY ='uPNlLvtXfhpnqQqEHuQx8jwwxvWUPW7ix0U9aIFGlcsiOdKYrd'
+API_KEY ='dVsDjo9XjsFY3KUJKxeN4GM8uDMl7PYxlxDn24uXDMZL63C77T'
 SECRET_KEY = b'dor9dor'
 
